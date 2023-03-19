66782435e8fa2ef647cc728da5528d1d1a06000f
m.ehab2 <muhamedehab1@gmail.com>
2023-03-19 07:12:36 +0200

    Initial commit

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
-
+const repeatString = function(string,int) {
+    let x=string;
+    let y=int;
+    let m=x;
+    if (y > 0) {
+        for (let index = 0; index < y - 1; index++) {
+            m = m + x;
+        }
+        return m;
+    }
+    else if(y===0){
+        return "";
+    }
+    else{
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
